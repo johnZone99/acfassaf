4a25d541379b2ef86a8719a98cbd0c0aea47724a
Johnzone7 <60849096+aymzone@users.noreply.github.com>
2023-12-03 10:26:17 +0000

    hhh

--- src/main/java/com/aymen/todolist/controller/TaskController.java
+++ src/main/java/com/aymen/todolist/controller/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200", "https://aymen-nacer.github.io/To-Do-List-App-with-User-Authentication/"})
 @RestController
 @RequestMapping("/api/todos")
 public class TaskController {
